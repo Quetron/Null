7093438f914349ad20971eae2ee441454ed7800a
amonq <060776@swcsd.us>
2022-01-27 10:39:44 -0500

    Exception Handling

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ Project 1
 package com.company;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -70,51 +71,58 @@ public class Main {
     }
 
     static void menuSelection() {
-        System.out.println("Menu:");
-        System.out.println("------------");
-        System.out.println("1: Add a task");
-        System.out.println("2: Remove a task");
-        System.out.println("3: Update a task");
-        System.out.println("4: See your current list of tasks");
-        System.out.println("0: Quit");
+        try{
+            System.out.println("Menu:");
+            System.out.println("------------");
+            System.out.println("1: Add a task");
+            System.out.println("2: Remove a task");
+            System.out.println("3: Update a task");
+            System.out.println("4: See your current list of tasks");
+            System.out.println("0: Quit");
 
-        //User input for which menu number they want to select
-        Scanner number = new Scanner(System.in);
-        System.out.print("Enter a number here: ");
-        int usrNumber = number.nextInt();
+            //User input for which menu number they want to select
+            Scanner number = new Scanner(System.in);
+            System.out.print("Enter a number here: ");
+            int usrNumber = number.nextInt();
 
-        switch (usrNumber) {
-            case 1:
-                addTask();
-                menuSelection();
-                break;
-            case 2:
-                removeTask();
-                menuSelection();
-                break;
-            case 3:
-                updateTask();
-                menuSelection();
-                break;
-            case 4:
-                System.out.println();
-                System.out.println("Here is your list:");
-                for (int i = 0; i < words.size(); i++) {
-                    System.out.println((i+1) + ": " + words.get(i));
-                }
-                System.out.println();
+            switch (usrNumber) {
+                case 1:
+                    addTask();
+                    menuSelection();
+                    break;
+                case 2:
+                    removeTask();
+                    menuSelection();
+                    break;
+                case 3:
+                    updateTask();
+                    menuSelection();
+                    break;
+                case 4:
+                    System.out.println();
+                    System.out.println("Here is your list:");
+                    for (int i = 0; i < words.size(); i++) {
+                        System.out.println((i+1) + ": " + words.get(i));
+                    }
+                    System.out.println();
 
-                menuSelection();
-                break;
-            case 0:
-                //Closes the program
-                System.out.println("Thanks!");
-                break;
+                    menuSelection();
+                    break;
+                case 0:
+                    //Closes the program
+                    System.out.println("Thanks!");
+                    break;
 
 
-            default:
-                System.out.println("Not a valid input, please input a number 0-5");
-                menuSelection();
+                default:
+                    System.out.println("Not a valid input, please input a number 0-5");
+                    menuSelection();
+            }
+
+        }
+        catch (InputMismatchException e){
+            System.out.println("That is not and integer, please enter an Integer.");
+            menuSelection();
         }
 
     }
